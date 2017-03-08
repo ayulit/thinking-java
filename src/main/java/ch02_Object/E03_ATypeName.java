5cf070035a302e7f94aaf464a2fbdd867f3f7321
ayulit <litwinoff@gmail.com>
2017-03-08 16:46:14 +0300

    02-03: New instance of Main class inside main()

---
+++ src/main/java/ch02_Object/E03_ATypeName.java
@@ -0,0 +1,13 @@
+package ch02_Object;
+
+public class E03_ATypeName {
+
+	public static void main(String[] args) {
+		
+		E03_ATypeName a = new E03_ATypeName();
+		
+		System.out.println(a.getClass().getName());
+
+	}
+
+}
