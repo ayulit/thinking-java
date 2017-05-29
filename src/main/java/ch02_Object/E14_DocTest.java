66c3d3a6fdb26a162007214cbaf65df225d2d104
ayulit <litwinoff@gmail.com>
2017-05-29 18:09:47 +0300

    02-14: Numbered HTML list in Javadoc

---
+++ src/main/java/ch02_Object/E14_DocTest.java
@@ -0,0 +1,28 @@
+package ch02_Object;
+
+
+/**
+ * A class comment
+ * Can be used unformatted HTML
+ * <pre>
+ * System.out.println("Hello, tomat!")
+ * </pre>
+ * */
+public class E14_DocTest {
+    
+    /** comment for a variable */
+    public int i;
+
+    /** comment for a <em>method</em>
+     *  
+     *  <b>Numbered list:</b>
+     *  <ol>
+     *      <li>One</li>
+     *      <li>Two</li>
+     *      <li>Three</li>
+     *  </ol>
+     **/
+    public void func() {
+    }
+
+}
