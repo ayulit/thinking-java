6487be61f24ad2967f4a501801e84836f25cb66b
ayulit <litwinoff@gmail.com>
2017-06-01 20:23:01 +0300

    03-09: Min Max Exponents

---
+++ src/main/java/ch03_Operators/E09_MinMaxExponents.java
@@ -0,0 +1,17 @@
+package ch03_Operators;
+
+import static net.mindview.util.Print.*;
+
+public class E09_MinMaxExponents {
+
+    public static void main(String[] args) {
+        
+        print("Float MIN: " + Float.MIN_VALUE);
+        print("Float MAX: " + Float.MAX_VALUE);
+        
+        print("Double MIN: " + Double.MIN_VALUE);
+        print("Double MAX: " + Double.MAX_VALUE);
+
+    }
+
+}
