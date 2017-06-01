7bec913e9a7f575d2b5b07ef1851b1bc219fe1db
ayulit <litwinoff@gmail.com>
2017-06-01 19:12:42 +0300

    03-07: Coin Flipping Simulator

---
+++ src/main/java/ch03_Operators/E07_CoinFlipping.java
@@ -0,0 +1,18 @@
+package ch03_Operators;
+
+import java.util.Random;
+
+public class E07_CoinFlipping {
+
+    public static void main(String[] args) {
+        
+        Random rand = new Random();
+        
+        boolean flip = rand.nextBoolean();
+        
+        System.out.print("OUTCOME: ");
+        System.out.println(flip ? "HEAD" : "TAIL");
+
+    }
+
+}
